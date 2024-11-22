8bf4f266668d3e00828e3a1494bf699f89665d21
vleantonov <volicance@gmail.com>
2024-11-22 19:16:58 +0300

    feat: add readme.md

--- config/config.js
+++ config/config.js
@@ -1,5 +1,8 @@
 export default {
+    // OpenCast host
     host: 'http://89.169.157.210/api/',
+    // Internal SSO Host
     hostToken: 'http://89.169.157.210/token/api/v0/debug_token',
+    // StreamController host
     streamHost: 'http://localhost:1984/api/'
 }
