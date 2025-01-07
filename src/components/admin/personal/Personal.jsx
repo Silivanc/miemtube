6e7272ae8ff5ae2caf55075440e0d685103cb1b8
Решетова Екатерина Юрьевна <eyureshetova_1@edu.hse.ru>
2025-01-07 18:14:55 +0300

    add table with users

--- src/components/admin/personal/Personal.jsx
+++ src/components/admin/personal/Personal.jsx
@@ -1,5 +1,30 @@
+import React from 'react';
+import './Personal.scss';
+
 export function Personal() {
     return (
-        <h2>Об авторе</h2>
+        <div>
+            <table>
+            <thead>
+                <tr>
+                <th>        </th>
+                <th>Имя</th>
+                <th>Дата регистрации</th>
+                <th>Действия</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                <td><button class="account-user"></button></td>
+                <td>Фамилия Имя</td>
+                <td>21 фев. 2015 г.</td>
+                <td>
+                    <button class="edit-user"></button>
+                    <button class="trash-user"></button>
+                </td>
+                </tr>
+            </tbody>
+            </table>
+        </div>
     )
 }
