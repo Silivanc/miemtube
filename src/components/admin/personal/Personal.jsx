44be3501f0b14f81ee9ed0932f4e94fbfc4e023c
Решетова Екатерина Юрьевна <eyureshetova_1@edu.hse.ru>
2025-01-07 21:47:17 +0300

    delete scss

--- src/components/admin/personal/Personal.jsx
+++ src/components/admin/personal/Personal.jsx
@@ -15,29 +15,29 @@ export function Personal() {
     }, []); // useEffect с пустым массивом зависимостей, запускается только один раз
 
     return (
-        <div className="p-1"> {/* Отступы вокруг таблицы */}
-            <table className="w-full border-collapse border border-gray-300">
+        <div className="p-0"> {/* Отступы вокруг таблицы */}
+            <table className="w-full border-none border-collapse border border-gray-300">
                 <thead>
                     <tr>
-                        <th className="p-3 border-b-2 border-gray-300 text-left">Имя</th>
-                        <th className="p-3 border-b-2 border-gray-300 text-left">Почта</th>
-                        <th className="p-3 border-b-2 border-gray-300 text-left">Действия</th>
+                        <th className="p-4 border-b-2 border-gray-300 text-left">Имя</th>
+                        <th className="p-4 border-b-2 border-gray-300 text-left">Почта</th>
+                        <th className="p-4 border-b-2 border-gray-300 text-left">Действия</th>
                     </tr>
                 </thead>
                 <tbody>
                     {users.length > 0 ? (
                         users.map((user, index) => (
                             <tr key={index} className="hover:bg-gray-50">
-                                <td className="p-3 border-b border-gray-200">{user.name}</td>
-                                <td className="p-3 border-b border-gray-200">{user.email}</td>
-                                <td className="p-3 border-b border-gray-200 flex gap-2">
+                                <td className="p-4 border-b border-gray-200">{user.name}</td>
+                                <td className="p-4 border-b border-gray-200">{user.email}</td>
+                                <td className="p-4 border-b border-gray-200">
                                     <button
-                                        className="w-5 h-5 bg-no-repeat bg-center bg-contain cursor-pointer"
-                                        style={{ backgroundImage: "url('/edit.svg')" }}
+                                        className="w-6 h- bg-no-repeat bg-center bg-contain cursor-pointer"
+                                        style={{ backgroundImage: "url('edit.svg')" }}
                                     />
                                     <button
-                                        className="w-5 h-5 bg-no-repeat bg-center bg-contain cursor-pointer"
-                                        style={{ backgroundImage: "url('/trash.svg')" }}
+                                        className="w-6 h-6 bg-no-repeat bg-center bg-contain cursor-pointer"
+                                        style={{ backgroundImage: "url('trash.svg')" }}
                                     />
                                 </td>
                             </tr>
